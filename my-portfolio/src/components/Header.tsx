2dcd11c933eb6f813be0549704b2714c82706c95
Mandisa <27103528@student.g.nwu.ac.za>
2025-03-19 15:43:33 +0200

    logo image

--- my-portfolio/src/components/Header.tsx
+++ my-portfolio/src/components/Header.tsx
@@ -1,11 +1,15 @@
 // src/components/Header.tsx
 import React from 'react';
+import Logo from '../assets/images/download.png'
 
 const Header: React.FC = () => {
   return (
     <header>
       <h1>My Portfolio</h1>
       <nav>
+      <img src={Logo} 
+       alt="Profile"
+       className="w-48 h-48 object-cover rounded-full border-4 border-blue-500 shadow-lg mx-auto" />
         <ul>
           <li><a href="#about">About Me</a></li>
           <li><a href="#projects">Projects</a></li>
